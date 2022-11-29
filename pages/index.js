03e5529f4959d901388558698427cc8778a20c07
nanoconsensus <nanoconsensus@gmail.com>
2022-11-29 20:01:52 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,15 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Let me be your coach</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Help me to help you</h2>
           </div>
+        </div>
+    {/* Add this code here*/}
+        <div className="prompt-container">
+          <textarea placeholder="start typing here" className="prompt-box" />
         </div>
       </div>
       <div className="badge-container grow">
